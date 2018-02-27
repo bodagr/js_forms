28f07dde2a55dc15d3bea4fed8a60f142da50b48
iQube.des <33438408+bodagr@users.noreply.github.com>
2018-02-26 20:44:02 -0600

    Add main files

---
+++ script.js
@@ -0,0 +1,21 @@
+// $(function(){
+// 	$.ajax({
+// 		url: 'https://gs-class.herokuapp.com/age.php',
+// 		method: 'post',
+// 		data: {
+// 			author: 'Ian',
+// 			text: 'Hello there!'
+// 		},
+// 		dataType: 'json',
+// 		success: function(data){
+// 			console.log(data);
+// 		},
+// 		error: function(response){
+// 			console.log(response);
+// 		}
+// 	})
+// })
+
+
+
+
